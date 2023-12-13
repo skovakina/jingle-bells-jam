445c8b5b3fe80561ff6948d0106dd2dfeb5ba960
Svetlana Kovakina <skovakina@gmail.com>
2023-12-13 11:30:36 -0800

    set up express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+// listen to port 3000
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  // if everything works fine, the console will show which port the application is listening to
+  console.log(`App listening at port ${PORT}`);
+});
