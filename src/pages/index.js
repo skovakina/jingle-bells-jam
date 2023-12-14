0111d5df946c0a9a778171f4992e48b9e784bbeb
Svetlana Kovakina <skovakina@gmail.com>
2023-12-14 14:55:33 -0800

    update images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,18 @@
 import "./index.css";
+
+// readJson () {
+//       fetch('/api/file')
+//    .then(response => {
+//        if (!response.ok) {
+//            throw new Error("HTTP error " + response.status);
+//        }
+//        return response.json();
+//    })
+//    .then(json => {
+//        this.users = json;
+//        //console.log(this.users);
+//    })
+//    .catch(function () {
+//        this.dataError = true;
+//    })
+// }
