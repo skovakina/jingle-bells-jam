897bc6e65411019f7fd097d8f7a9e2bdd29f174f
Svetlana Kovakina <skovakina@gmail.com>
2023-12-14 19:01:12 -0800

    add map and santa's route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,38 @@
 import "./index.css";
+import data from "../scripts/data.json";
+import FormValidator from "../components/FormValidator.js";
 
-// readJson () {
-//       fetch('/api/file')
-//    .then(response => {
-//        if (!response.ok) {
-//            throw new Error("HTTP error " + response.status);
-//        }
-//        return response.json();
-//    })
-//    .then(json => {
-//        this.users = json;
-//        //console.log(this.users);
-//    })
-//    .catch(function () {
-//        this.dataError = true;
-//    })
-// }
+console.log(data);
+
+const map = L.map("map").setView([51.505, -0.09], 2);
+const path = [];
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+data.countries.forEach((item) => {
+  L.marker([item.latitude, item.longitude])
+    .addTo(map)
+    .bindPopup(item.name)
+    .openPopup();
+  path.push([item.latitude, item.longitude, item.optimized_position]);
+});
+
+const optimalPath = path.sort((a, b) => a[2] - b[2]);
+const polyline = L.polyline(optimalPath, { color: "red" }).addTo(map);
+
+const form = document.forms.form;
+
+const options = {
+  formSelector: ".form",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__button",
+  inactiveButtonClass: "popup__btn-submit_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error_active",
+};
+
+// const formValidator = new FormValidator(options, form);
+// formValidator.enableValidation();
