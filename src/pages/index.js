43929624aa91074f2dc31e6088e276b8f920e1c4
Svetlana Kovakina <skovakina@gmail.com>
2023-12-14 22:00:51 -0800

    fix placeholders

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import santa from "../images/santa.png";
 import data from "../scripts/data.json";
 import FormValidator from "../components/FormValidator.js";
 
@@ -12,8 +13,15 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+const santaIcon = L.icon({
+  iconUrl: santa,
+  iconSize: [35, 48],
+  iconAnchor: [10, 20],
+  popupAnchor: [5, -20],
+});
+
 data.countries.forEach((item) => {
-  L.marker([item.latitude, item.longitude])
+  L.marker([item.latitude, item.longitude], { icon: santaIcon })
     .addTo(map)
     .bindPopup(item.name)
     .openPopup();
@@ -21,7 +29,7 @@ data.countries.forEach((item) => {
 });
 
 const optimalPath = path.sort((a, b) => a[2] - b[2]);
-const polyline = L.polyline(optimalPath, { color: "red" }).addTo(map);
+const polyline = L.polyline(optimalPath).addTo(map);
 
 const form = document.forms.form;
 
--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = {
           },
         ],
       },
+
       {
         test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/,
         type: "asset/resource",
